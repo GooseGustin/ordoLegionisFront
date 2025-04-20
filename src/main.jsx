497392df64ed9999264fc7e39f4034d8e598bb0e
Pam <pammafeng11@gmail.com>
2025-04-20 15:06:50 +0100

    Trying Render for deployment. Removed link to local bootstrap from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
-import '../../../../resources/bootstrap-5.2.0-beta1/dist/css/bootstrap.css'
-import '../../../../resources/fontawesome-6.7.1/css/fontawesome.min.css'
+// import '../../../../resources/bootstrap-5.2.0-beta1/dist/css/bootstrap.css'
+// import '../../../../resources/fontawesome-6.7.1/css/fontawesome.min.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
